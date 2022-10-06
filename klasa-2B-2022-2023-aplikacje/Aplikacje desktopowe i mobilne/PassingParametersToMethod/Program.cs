da689f5e99bfc63231137bee90d3ac9b27d6d32a
Gabriel Achler <112950410+2rnu@users.noreply.github.com>
2022-10-06 13:16:52 +0200

    Add files via upload

---
+++ klasa-2B-2022-2023-aplikacje/Aplikacje desktopowe i mobilne/PassingParametersToMethod/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace PassingParametersToMethod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TestingParamteters testingParamteters = new TestingParamteters();
+            //testingParamteters.Test1();
+            testingParamteters.Test2();
+        }
+    }
+}
