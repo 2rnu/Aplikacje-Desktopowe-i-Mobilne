797a3834789a9290e87674d981d54d659e57fb40
Gabriel Achler <112950410+2rnu@users.noreply.github.com>
2022-10-11 14:27:01 +0200

    Add files via upload

---
+++ Aplikacje-Desktopowe-i-Mobilne/Aplikacje desktopowe i mobilne/001 - 08.09.2022 - Projekt nr.1/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace _001___08._09._2022___Projekt_nr._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+    }
+}
---
+++ Aplikacje-Desktopowe-i-Mobilne/klasa-2B-2022-2023-aplikacje/Aplikacje desktopowe i mobilne/PolymorphismApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PolymorphismApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
