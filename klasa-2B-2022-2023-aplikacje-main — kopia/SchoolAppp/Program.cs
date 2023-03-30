df41f26d68d55d757eb2ce8ed63ba44843d8eb69
Gabriel Achler <112950410+2rnu@users.noreply.github.com>
2023-03-30 13:14:37 +0200

    Add files via upload

---
+++ klasa-2B-2022-2023-aplikacje-main — kopia/SchoolAppp/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace SchoolApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //DRUD
+            Console.WriteLine("Hello World!");
+
+        }
+    }
+}
