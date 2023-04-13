6b6684524b2546dbaf524d677ad9b2c70c548ec2
Gabriel Achler <112950410+2rnu@users.noreply.github.com>
2023-04-13 07:40:23 +0200

    Add files via upload

---
+++ Aplikacje/Aplikacje desktopowe i mobilne/SchoolAppp/Database/Entitis/SchoolClasses.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SchoolApp.Database.Entitis
+{
+    class SchoolClasses
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+    }
+}
---
+++ Aplikacje/Aplikacje desktopowe i mobilne/SchoolAppp/Database/SchoolDatabaseContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace SchoolApp.Database
+{
+    class SchoolDatabaseContext : DbContext
+    {
+        public DbSet<SchoolClass> SchoolClasses { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite("FileName = SchoolDatabase.db");
+            base.OnConfiguring(optionsBuilder);
+        }
+    }
+}
