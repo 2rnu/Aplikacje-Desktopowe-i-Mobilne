c5d37c8ca7d7b953d5dde6108a1b2bc4c141a292
Uczeń - sala 213 <Ucze - sala 213@213-13>
2023-10-09 16:42:22 +0200

    Aktualizacja z dnia 9.10.2023

---
+++ Aplikacje 2023-2024/Aplikacje desktopowe i mobilne/Notatka/Notatka.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+class Notatka
+{
+    static void Main(string[] args)
+    {
+        Notatka notatka1 = new Notatka("Notatka 1", "Treść notatki 1");
+        Notatka notatka2 = new Notatka("Notatka 2", "Treść notatki 2");
+
+        notatka1.Wyswietlanie_tytulu_tresci();
+        notatka2.Wyswietlanie_tytulu_tresci();
+
+        notatka1.metoda_diagnostyczna();
+        notatka2.metoda_diagnostyczna();
+
+        Console.ReadKey();
+    }
+}
+
+public class Notatka
+{
+    private static uint SumaNotatek = 0;
+    private uint identyfikator;
+    private string tytul;
+    private string tresc;
+
+    public Notatka(string tytul, string tresc)
+    {
+        SumaNotatek++;
+        identyfikator = SumaNotatek;
+        this.tytul = tytul;
+        this.tresc = tresc;
+    }
+
+    public void Wyswietlanie_tytulu_tresci()
+    {
+        Console.WriteLine($"tytul: {tytul}");
+        Console.WriteLine($"tresc: {tresc}");
+    }
+
+    public void metoda_diagnostyczna()
+    {
+        Console.WriteLine($"Identyfikator: {identyfikator}; Tytuł: {tytul}; tresc: {tresc}");
+    }
+}
+
+/*************************************************
+ klasa: Notatka
+ opis: program implementujący klasę do obsługi notatek
+ pola1: pole maoin ma za zadanie wyświetlić nazwę noatatki
+ pole2L: public class ma za zadanie utworzyć notatkę
+ Pola: 3
+ autor: Gabriel Achler
+*************************************************/
